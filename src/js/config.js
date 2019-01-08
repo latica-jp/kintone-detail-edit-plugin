b16940d9f98281513778e9db54c7a0a51aae053a
latica-jp <latica.jp@gmail.com>
2019-01-08 12:04:02 +0900

    first commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,21 +1,25 @@
-// jQuery は webpack がロード
+// jQuery は webpack でロードしている
 const PLUGIN_ID = kintone.$PLUGIN_ID;
 
 const $form = $('.js-submit-settings');
-const $message = $('.js-text-message');
+const $checkbox = $('#js-checkbox-enabled');
 
 const getSettingsUrl = () => {
   return '/k/admin/app/flow?app=' + kintone.app.getId();
 };
 
 const config = kintone.plugin.app.getConfig(PLUGIN_ID);
-if (config.message) {
-  $message.val(config.message);
-}
+// 設定文字列を boolean に変換
+$checkbox.prop('checked', config.enabled == 'true');
+
 $form.on('submit', e => {
   e.preventDefault();
-  kintone.plugin.app.setConfig({ message: $message.val() }, () => {
-    alert('Please update the app!');
-    window.location.href = getSettingsUrl();
-  });
+  kintone.plugin.app.setConfig(
+    // 設定は文字列で保存
+    { enabled: $checkbox.prop('checked') ? 'true' : 'false' },
+    () => {
+      alert('アプリを更新してプラグイン設定を反映してください');
+      window.location.href = getSettingsUrl();
+    }
+  );
 });
--- src/js/desktop.js
+++ src/js/desktop.js
@@ -1,20 +1,38 @@
-// jQuery は webpack がロード
+// jQuery は webpack でロードしている
 const PLUGIN_ID = kintone.$PLUGIN_ID;
 
-kintone.events.on('app.record.index.show', () => {
-  var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+kintone.events.on('app.record.index.show', event => {
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID);
+  if (config.enabled !== 'true') return;
 
-  var spaceElement = kintone.app.getHeaderSpaceElement();
-  var fragment = document.createDocumentFragment();
-  var headingEl = document.createElement('h3');
-  var messageEl = document.createElement('p');
+  // いちばん左の列の横幅を拡げる
+  $('#view-list-data-gaia th:first-child').attr({ style: 'width: 64px;' });
 
-  messageEl.classList.add('plugin-space-message');
-  messageEl.textContent = config.message;
-  headingEl.classList.add('plugin-space-heading');
-  headingEl.textContent = 'Hello kintone plugin!';
+  // 詳細画面へのリンクを含むセル（＝いちばん左の列のセル）の配列を取得
+  const $detailColumsCells = $('td[class*="detail-action-"]');
+  // 詳細編集画面へのリンクボタンを追加する
+  $detailColumsCells.each((_, element) => {
+    const $detailEditLink = createDetailEditLinkButton(element);
+    $(element).append($($detailEditLink));
+  });
 
-  fragment.appendChild(headingEl);
-  fragment.appendChild(messageEl);
-  spaceElement.appendChild(fragment);
+  return event;
 });
+
+/**
+ * 詳細編集画面へのリンクボタンを作成する
+ * @element {object} 詳細画面へのリンクを収容するセル
+ */
+const createDetailEditLinkButton = element => {
+  // 詳細編集画面へのリンクを作成
+  const $detailEditLink = $('<a></a>').append(
+    $('<span></span>', { class: 'recordlist-edit-icon-gaia' })
+  );
+  // 詳細画面へのリンクの href を取得する
+  const $detailLinkHref = $(element)
+    .find('a.recordlist-show-gaia')
+    .attr('href');
+  // 詳細編集画面へのリンクの href を生成してリンクに適用する
+  $detailEditLink.attr({ href: `${$detailLinkHref}&mode=edit` });
+  return $detailEditLink;
+};
